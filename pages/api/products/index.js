99409f8493714e72a6e776e10961ca2384c53162
Sheb <michael@sheb.rocks>
2022-10-28 09:07:32 +0200

    reset version

--- pages/api/products/index.js
+++
@@ -1,8 +0,0 @@
-import { getProducts } from "../../../helpers/api"
-
-function handler(req, res) {
-    const products = getProducts()
-    res.status(200).json(products)
-}
-
-export default handler
--- pages/products/[productId]/index.js
+++ pages/products/[productId]/index.js
@@ -1,18 +1,14 @@
 import { useRouter } from "next/router";
 
-import { getProductById } from "../../../helpers/api";
-
 function ProductDetail() {
   const router = useRouter();
   const { productId } = router.query;
 
-  const product = getProductById(productId);
 
   return (
     <>
       <article>
         <h1>Product Detail for: {productId}</h1>
-        <p>{product.name}</p>
       </article>
     </>
   );
