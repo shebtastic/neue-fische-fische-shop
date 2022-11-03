60ba838b35d6d614afce3c5d48a22703a337742f
Sheb <michael@sheb.rocks>
2022-11-03 10:38:22 +0100

    introduced endpoint that matches api

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,8 +1,13 @@
 import { getAllProducts } from "../../../helpers/api";
 
 function handler(request, response) {
-    const products = getAllProducts()
-    response.status(200).json(products)
+    console.log(request.method)
+    if(request.method === "GET") {
+        const products = getAllProducts()
+        response.status(200).json(products)
+    } else {
+        response.status(405).setHeader('Allow', ['GET']).send("Hier gibts den endpoint nicht mit der methode.")
+    }
 }
 
 export default handler
